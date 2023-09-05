ea063f2a4661c6cc6fd6520dc546f1ba8a9afe58
angelo <angelo2002017@gmail.com>
2023-09-05 12:32:52 +0200

    First try to make tic tac toe object oriented, UNDER DEVELOPMENT

--- Game.java
+++ Game.java
@@ -21,28 +21,49 @@ public class Game {
                 //0,0
                 if(map[1][0] == s && map[2][0] == s) return true; //down
                 if(map[0][1] == s && map[0][2] == s) return true; //right
-                if(map[1][1] == s && map[2][2] == s) return true; //diagonal
+                return map[1][1] == s && map[2][2] == s; //diagonal
 
             }else if(c[1] == 1){
                 //0,1
+                if(map[1][1] == s && map[2][1] == s) return true; //down
+                return map[0][0] == s && map[0][2] == s; //right left
             }else if(c[1] == 2){
                 //0,2
+                if(map[1][2] == s && map[2][2] == s) return true; //down
+                if(map[0][0] == s && map[0][1] == s) return true; //left
+                return map[1][1] == s && map[2][0] == s; //diagonal
             }
         }else if(c[0] == 1){
             if(c[1] == 0){
-                //0,0
+                //1,0
+                if(map[0][0] == s && map[2][0] == s) return true; //up down
+                return map[1][1] == s && map[1][2] == s; //right
             }else if(c[1] == 1){
-                //0,1
+                //1,1
+                if(map[0][1] == s && map[2][1] == s) return true; //up down
+                if(map[1][0] == s && map[1][2] == s) return true; //right left
+                if(map[0][0] == s && map[2][2] == s) return true; //diagonal 1
+                return map[2][0] == s && map[0][2] == s; //diagonal 2
             }else if(c[1] == 2){
-                //0,2
+                //1,2
+                if(map[0][2] == s && map[2][2] == s) return true; //up down
+                return map[1][1] == s && map[1][0] == s; //left
             }
         }else if(c[0] == 2){
             if(c[1] == 0){
-                //0,0
+                //2,0
+                if(map[1][0] == s && map[0][0] == s) return true; //up
+                if(map[2][1] == s && map[2][2] == s) return true; //right
+                return map[1][1] == s && map[0][2] == s; //diagonal
             }else if(c[1] == 1){
-                //0,1
+                //2,1
+                if(map[1][1] == s && map[0][1] == s) return true; //up
+                return map[2][0] == s && map[2][2] == s; //right left
             }else if(c[1] == 2){
-                //0,2
+                //2,2
+                if(map[1][2] == s && map[0][2] == s) return true; //up
+                if(map[2][1] == s && map[2][0] == s) return true; //left
+                return map[1][1] == s && map[0][0] == s; //diagonal
             }
         }
         return false;
@@ -59,7 +80,6 @@ public class Game {
             coordinates[1] = scan.nextInt();
 
             moved = hand.move(p,coordinates,map);
-            //ADD CONTROL OF ILLEGAL MOVE
 
 
             for(int i = 0; i<3; i++){
@@ -70,6 +90,11 @@ public class Game {
                 System.out.print(map[i][2]);
                 System.out.println();
             }
+            //controllo vittoria
+            if(won(p,coordinates)) {
+                System.out.println("VINTO");
+                finish = true;
+            }
             //cambio giocatore
             if (p.getId() == 0){
                 p = p2;
@@ -77,14 +102,17 @@ public class Game {
                 p = p1;
             }
 
-            //controllo vittoria
-            if(won(p,coordinates)) {
-                System.out.println("VINTO");
-                finish = true;
-            }
             countMove ++;
             if(countMove == 9) finish = true;
         }
-        //vittoria di qualcuno o stallo?
+        /*vittoria di qualcuno o stallo?
+        if(finish ){
+            System.out.print("HA VINTO: ");
+            System.out.print(p.getName());
+        }else if{
+            System.out.print("STALLO");
+        }
+
+         */
     }
 }
--- Move.java
+++ Move.java
@@ -1,4 +1,5 @@
 public class Move {
+    //ADD CONTROL OF ILLEGAL MOVE
     public boolean move(Player p, int[] c, char[][] m){
         if(m[c[0]][c[1]] != ' ') return false;
         m[c[0]][c[1]] = p.getSign();
